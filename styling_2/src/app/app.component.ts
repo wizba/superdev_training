6c2a7526112f2df660597e72433e411ea62e9e8f
william <williamrabs@gmail.com>
2020-04-25 14:49:52 +0200

    changes done

--- styling_2/src/app/app.component.ts
+++ styling_2/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mdb-angular-free';
+
+
+  elements: any = [
+    {id: 1, first: 'Mark', last: 'Otto', handle: '@mdo'},
+    {id: 2, first: 'Jacob', last: 'Thornton', handle: '@fat'},
+    {id: 3, first: 'Larry', last: 'the Bird', handle: '@twitter'},
+  ];
+
+  headElements = ['ID', 'First', 'Last'];
 }
